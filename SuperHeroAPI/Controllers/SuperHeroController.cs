b5967c3a89b4e85ccc36e05cd09084d270f3882d
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-15 22:55:39 +0300

    upd getHeroes

--- SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI.Data;
 
 namespace SuperHeroAPI.Controllers
 {
@@ -7,21 +9,18 @@ namespace SuperHeroAPI.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public SuperHeroController(DataContext context)
+        {
+            _context = context;
+        }
+
+
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
         {
-            return new List<SuperHero> { 
-                
-                new SuperHero
-               {
-                    Name="Spider Man",
-                    FirstName="Peter",
-                    LastName="Parker",
-                    Place="New York City"
-
-               }
-          
-          };
+            return Ok(await _context.SuperHeroes.ToListAsync());
         }
     }
 }
